2a206c467880772e8762e9aef7ef15d64c000dba
Amrampro <gouliben@gmail.com>
2025-04-11 01:08:34 +0200

    edited file vite.config for local use

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/icc_welcome/",
+  // base: "/icc_welcome/", // Uncomment this line if you are deploying to GitHub Pages
   plugins: [react()],
 })
