e6388debd3d5d157ad44e25c1db76b641e0de9aa
Amrampro <gouliben@gmail.com>
2025-04-12 22:07:39 +0200

    updated bug

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import githubPages from 'vite-plugin-github-pages'; 
+import githubPages from 'vite-plugin-github-pages';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   // plugins: [react()],
   plugins: [react(), githubPages()],
-  base: "/icc_welcome/", // Uncomment this line if you are deploying to GitHub Pages
+  // base: "/icc_welcome/", // Uncomment this line if you are deploying to GitHub Pages
 })
