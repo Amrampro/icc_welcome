00ea5be60b7e49c5ad121fce108212807f918d6b
Amrampro <gouliben@gmail.com>
2025-04-18 16:05:39 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className="bottom-0 mt-6 left-0 w-full z-10 bg-gray-800">
       <div className="container mx-auto flex gap-8 items-center py-4 px-6 md:px-20 lg:px-32">
         <p className="text-white">
-          © Impact Centre Chrétien - Charleroi {currentYear} <span className="mx-2">•</span> Tous droits réservés
+          © Impact Centre Chrétien - Charleroi {currentYear} <span className="mx-2">•</span> Tous droits réservés 
         </p>
       </div>
     </div>
